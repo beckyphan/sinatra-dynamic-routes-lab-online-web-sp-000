3f5d4a51a9d7b8020b8980c10597c5fa2ce1d687
Becky Phan <beckyphan@live.com>
2019-08-12 20:57:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
   end 
   
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  end 
+  
   get '/:operation/:number1/:number2' do
   end 
 
