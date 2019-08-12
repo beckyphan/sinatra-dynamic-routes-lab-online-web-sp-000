89278ff1dab1961a51d3ba133ece6927f90a83fe
Becky Phan <beckyphan@live.com>
2019-08-12 21:01:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i 
     @number2= params[:number2].to_i
     
-    params[:operation] == 'add'
+    case params[:operation]
       "#{@number1 + @number2}"
     
   end 
